3ac67cbd28e945b8c1bfc27d2ebb36622f39f1f1
Alperen <alpyurtseven99@gmail.com>
2019-12-03 14:15:38 +0300

    v0.a0

--- HelloWorldML/DataForML/data.py
+++ HelloWorldML/DataForML/data.py
@@ -1,35 +1,35 @@
-import  requests
-from bs4 import BeautifulSoup
-import json
-import os
-import csv
+import  requests #HTTP Sorgularını yaparken kullancağımız kütüphane
+from bs4 import BeautifulSoup# HTML Parser
+import json  # verisetini oluşturmak için
+import os # dosya işlemleri için
+import csv # verisetini oluşturmak için
 
 class main:
     def __init__(self):
-        cname=input("classname")
-        element=input("element")
-        wb = input("webaddresi")
-        req(cname,element,wb)
+        cname=input("classname")  #html parse ederken kullancağımız varsa class ismi
+        element=input("element") #html parse ederken kullancağımız element ismi ("div","p"vs)
+        wb = input("webaddresi") # sorgu yapacağımız ve parse edeceğimiz websitesi
+        req(cname,element,wb) #sorgu yapacağımız metod
 def req(cn,el,webaddress):
-    r = requests.get(webaddress)
-    sc = r.status_code
-    content = r.content
-    bsoup(cn,el,content)
+    r = requests.get(webaddress) #get metoduyla sayfadan html i çektik
+    sc = r.status_code # web sitesinin bize verdiği cevap ("200","403","404" vs)
+    content = r.content #html içeriği
+    bsoup(cn,el,content) #htmli parse edeceğimiz metod
 def bsoup(cn,et,rcont):
-    soup= BeautifulSoup(rcont,"lxml")
-    dataset = soup.find_all(et,attrs={"class":cn})
+    soup= BeautifulSoup(rcont,"lxml") #BeautifulSoup kütüphanesini soup ile çağırıyoruz rcont html içeriğimiz
+    dataset = soup.find_all(et,attrs={"class":cn}) #find all ile belirttiğimiz class ismine ve belirttiğimiz elemente sahip lanhtml içeriğinin tamamını alıyoruz
     m2list= {}
     sayac =0
-    for i in dataset:
-        m2 = i.h4.text
-        price = i.h3.text
+    for i in dataset: #istediğimiz içerik içinde
+        m2 = i.h4.text #h4 olanların texti ör:metrekare
+        price = i.h3.text #h3 olanların texti ör:fiyat
         cur_path = os.path.dirname(__file__)
-        new_path = os.path.relpath('..\\MLFiles\\dataset.csv', cur_path)
+        new_path = os.path.relpath('..\\MLFiles\\dataset.csv', cur_path) #csv dosyasının pathini belirtiyoruz
         with open(new_path, 'a', newline='') as file:
             writer = csv.writer(file)
-            if sayac==0:
+            if sayac==0: #ilk seferinde başlıkları yazdır
                 writer.writerow(["SN", "m2", "price"])
-            writer.writerow([sayac, m2, price])
+            writer.writerow([sayac, m2, price]) #çektiğimiz içeriği csv ye yazdır
 
         #m2list[sayac]= {"0":m2,"1":price}
         sayac=sayac+1
--- HelloWorldML/MLFiles/MLProject.py
+++ HelloWorldML/MLFiles/MLProject.py
@@ -1,30 +1,29 @@
-import numpy as np
-from sklearn.linear_model import LinearRegression as lr
-import pandas as pd
-import matplotlib.pyplot as plt
+import numpy as np #matematiksel işlemlerde kullancağımız kütüphane
+from sklearn.linear_model import LinearRegression as lr #Sklearn linner regresyon modeli
+import pandas as pd # verileri düzenlemek için kullanılan kütüphane
+import matplotlib.pyplot as plt  #grafik çizdirmek için kullanılan kütüphane
 class main():
     def __init__(self):
         super().__init__()
-        dataset = np.array([[1, 1], [1, 2], [2, 2], [2, 3]])
         readdata()
 
-def readdata():
+def readdata():  # verileri çekip düzenleyeceğimiz metod
     data = pd.read_csv("dataset.csv")
-    x= data["m2"]
-    y= data["price"]
-    x= x.values.reshape(6,1)
-    y=y.values.reshape(6,1)
-    linear_regression(x,y)
+    x= data["m2"] #dataset csv içindeki başlıklar
+    y= data["price"] #dataset csv içindeki başlıklar
+    x= x.values.reshape(6,1) #verisetini yeniden boyutlandırıyoruz (6 adet x satırı  olduğu için)
+    y=y.values.reshape(6,1) #verisetini yeniden boyutlandırıyoruz (6 adet x satırı  olduğu için)
+    linear_regression(x,y)  #kendi oluşturduğumuz lineer regresyon metodunu çağırıyoruz
 
 def linear_regression(x,y):
-    lineerreg  = lr()
-    lineerreg.fit(x,y)
-    lineerreg.predict(x)
-    m=lineerreg.coef_
-    b=lineerreg.intercept_
-    plt.scatter(x, y)
-    plt.plot(x,lineerreg.predict(x),c="red")
-    plt.show()
+    lineerreg  = lr()  #sklearn lineer regresyon modelini 'lineerreg' adıyla kullancağız
+    lineerreg.fit(x,y)  # örneğin veri üzerinde öğrenmesi fit fonksiyonuyla yapılıyor
+    lineerreg.predict(x)  #tahmin fonksiyoru
+    m=lineerreg.coef_ #eğim
+    b=lineerreg.intercept_ #b değeri
+    plt.scatter(x, y) # matplotlib ile noktaları gösterme
+    plt.plot(x,lineerreg.predict(x),c="red") # doğruyu çizdirme
+    plt.show() # çizilen grafiği göster
 
 
 main()
